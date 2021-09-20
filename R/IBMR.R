d2271d289e350889e2340a704c0269e7139d97c3
Keshav Motwani <keshavmot2@gmail.com>
2021-09-20 15:49:03 -0400

    maybe fix bug

--- R/IBMR.R
+++ R/IBMR.R
@@ -265,12 +265,12 @@ IBMR_no_Gamma_subset = function(Y_list,
 
   print(paste0("Keeping ", sum(sapply(indices_list, length)), " observations out of a total of ", sum(sapply(Y_list, length))))
 
-  Y_list = mapply(Y = Y_list, indices = indices_list, map = category_mappings, FUN = function(Y, indices, map) unlist(map[Y[indices]]), SIMPLIFY = FALSE)
-  X_list = mapply(X = X_list, indices = indices_list, FUN = function(X, indices) X[indices, ], SIMPLIFY = FALSE)
+  Y_list = list(unlist(mapply(Y = Y_list, indices = indices_list, map = category_mappings, FUN = function(Y, indices, map) unlist(map[Y[indices]]), SIMPLIFY = FALSE)))
+  X_list = list(do.call(rbind, mapply(X = X_list, indices = indices_list, FUN = function(X, indices) X[indices, ], SIMPLIFY = FALSE)))
 
   IBMR_no_Gamma(Y_list,
                 categories,
-                replicate(length(Y_list), as.list(setNames(nm = categories)), simplify = FALSE),
+                list(as.list(setNames(nm = categories))),
                 X_list,
                 Y_list_validation,
                 category_mappings_validation,
