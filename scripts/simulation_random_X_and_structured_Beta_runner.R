4ffaadbd8c7c7010ea1322b98784dfe65bbb68b2
Keshav Motwani <keshavmot2@gmail.com>
2021-10-26 11:53:57 -0400

    update final sims hopefully

--- scripts/simulation_random_X_and_structured_Beta_runner.R
+++ scripts/simulation_random_X_and_structured_Beta_runner.R
@@ -3,20 +3,20 @@ library(IBMR)
 source("scripts/simulation_setup.R")
 
 ARRAY_ID = as.numeric(Sys.getenv('SLURM_ARRAY_TASK_ID'))
-RESULT_PATH = "final_results/simulations_random_X_and_structured_Beta_updated_again_more_fine_more_nonzero_more_fine_sigma1_catmap62_changeNprange"
+RESULT_PATH = "final_results/simulations_random_X_and_structured_Beta_final"
 dir.create(RESULT_PATH, recursive = TRUE)
 
 methods = c("IBMR_int", "IBMR_no_Gamma", "subset", "relabel")
 methods = c(methods, paste0(methods, "_ORC_fine")) # , paste0(methods, "_ORC_clean"), paste0(methods, "_ORC_fine_clean"))
 
 defaults = list(
-  # 6 coarse categories, each split into 2 subcategories. 6 datasets, 3 observed at coarse resolution, 3 at fine resolution for all categories.
+  # 6 coarse categories, each split into 2 subcategories. 6 datasets, 4 observed at coarse resolution in categories 1-10 and fine resolution in categories 11-12, 2 at fine resolution for all categories.
   category_mappings = simulate_category_mappings(2, c(6, 2), c(replicate(4, c(rep(1, 5), 2), simplify = FALSE), replicate(2, rep(2, 6), simplify = FALSE))),
   N = 4800,
   p = 500,
   nonzero = 100,
   s = 40, # number of genes which have shared coefficients within coarse categories
-  sigma = 1, # sd of normal distribution for sampling coefficients
+  sigma = 2, # sd of normal distribution for sampling coefficients
   rank = "int",
   batch_effect = 0.1 # norm of batch effect over norm of true predictors
 )
@@ -24,8 +24,8 @@ defaults = list(
 considered_values = list(
   p = c(250, 500, 1000, 2000),
   s = c(0, 20, 40, 60, 80),
-  N = c(1200, 2400, 4800, 9600, 19200),
-  batch_effect = c(0.025, 0.05, 0.1, 0.2, 0.4)
+  N = c(2400, 4800, 9600, 19200),
+  batch_effect = c(0, 0.025, 0.05, 0.1, 0.2, 0.4)
 )
 
 parameters = expand_parameters("random_X_and_structured_Beta", considered_values, defaults, 50, methods)
