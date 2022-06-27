5e4573c827011e946e13bd1252b0a1b754bc678a
Keshav Motwani <keshavmot2@gmail.com>
2022-06-27 13:44:25 -0400

    fix validation

--- scripts/simulation_setup.R
+++ scripts/simulation_setup.R
@@ -649,6 +649,8 @@ fit_SingleR = function(data) {
 
       fit = trainSingleR(ref = lapply(subsetted$X_list, t), labels = subsetted$Y_list, de.method = "wilcox", aggr.ref = TRUE, de.n = de.n)
 
+      P_list_validation = list()
+
       for (k in 1:length(data$validation$Y_list)) {
 
         predictions = classifySingleR(t(data$validation$X_list[[k]]), fit, quantile = quantile)$labels
